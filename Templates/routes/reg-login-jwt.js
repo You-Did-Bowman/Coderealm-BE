a49b5807d9dbc368d4f13c64d857cd4c0d4b4d86
Avdyl <avdyl.zekaj@dci-student.org>
2025-06-23 09:57:59 +0200

    deploymentBE

--- Templates/routes/reg-login-jwt.js
+++
@@ -1,8 +0,0 @@
-const router = express.Router();
-
-router.post("/register", user_validations, registerUser);
-router.post("/login", loginValidation, loginUser);
-router.get("/verify/:token", verifyUser);
-
-
-export default router;
